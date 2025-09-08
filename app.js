9650743ca3b0fc02bbbdbb9d444c59434b254f83
Reevan D Mello <reevandmello@gmail.com>
2025-09-08 21:37:46 +0530

    Blog basic functionality implemented successfully in node.js

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
-const express = require('express')
-
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes.js')
 const app = express();
 
-app.listen(3000);
+const dbURI = 'mongodb+srv://reevans-blogging:test123@cluster0.3lse2nv.mongodb.net/reevans-blog';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+app.set('view engine', 'ejs');
+app.set('views','pages');
+
+//middleware
+app.use(morgan('common'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+app.get('/all-blogs', (req,res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
 
 app.get('/', (req, res) => {
-    res.sendFile('./pages/index.html',{ root: __dirname});
+    res.redirect('/blogs');
 });
 
-app.get('/payment', (req,res) => {
-    res.sendFile('./pages/payment.html',{ root: __dirname});
+app.get('/about', (req,res) => {
+    res.render('about', {title: 'About'});
 });
 
-app.get('/pay', (req,res) => {
-    res.redirect('/payment');
-});
+//blog routes
+app.use('/blogs',blogRoutes);
+
 
 app.use((req, res) => {
-    res.status(404).sendFile('./pages/404.html', {root: __dirname});
+    res.status(404).render('404', {title: '404'});
 });
